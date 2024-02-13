657a1b72942feefe7b753cbd7fae80ad75005113
chayan chowdhury <chayanchowdhury43@yahoo.com>
2024-02-13 22:14:50 +0530

    Initialize variable string properties and function

---
+++ src/PropertiesAndFucntionInString.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println("Properties and Function In kotlin")
+    val str="WelcomeToKotlin"
+    println("String is:${str}")
+    val str1="WelcomeToKotlin"
+    println("Length is:${str.length}")
+    println("Get Item:${str[3]}")
+    println("Compare String: ${str.compareTo(str1)}")
+    println("Subsequence is:${str.subSequence(1,6)}")
+}
---
+++ src/VariableDeclaration.kt
@@ -0,0 +1,27 @@
+fun main() {
+
+    //Normal Way to Declare variable
+
+    var name="Mamata"
+    var age=32
+    var gender=true
+    var salary=2000
+
+    println("Name ::  ${name}")
+    println("Age ::  ${age}")
+    println("gender ::  ${gender}")
+    println("salary ::  ${salary}")
+
+    //Other Way to Declare variable
+
+    var empName:String="Modi"
+    var empAge:Int=30
+    var empGender:Boolean=false
+    var empSalary:Float=5000f
+
+    println("Employee Name: ${empName}")
+    println("Age:${empAge}")
+    println("EmpGender:${empGender}")
+    println("EmpSalaray:${empSalary}")
+
+}
