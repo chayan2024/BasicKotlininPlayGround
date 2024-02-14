2a65ae2f43ad901ab52eb8f05c32dc1a5123957a
chayan chowdhury <chayanchowdhury43@yahoo.com>
2024-02-14 23:32:06 +0530

    Initialize variable string properties and function How to get access all the element from String Program

---
+++ src/StringSimpleProgram.kt
@@ -0,0 +1,27 @@
+fun main (){
+
+    println("How to get access all the element from String")
+
+    var str="Hello"
+    println(str[0])
+    println(str[1])
+    println(str[2])
+    println(str[3])
+    println(str[4])
+
+    // Other way to get all the element
+
+    var str1="Kotlin"
+
+    // apprracoh-1
+
+    for(i in 0..str1.length-1){
+        println("Elements:  ${str1[i]}")
+    }
+
+    // apprracoh-2
+
+   for(i in str1.indices){
+       println("Element Value:${str1[i]}")
+   }
+}
