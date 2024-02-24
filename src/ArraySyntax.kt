a76c178fbfc11f76b3968eb5b32af2faefe7081e
chayan chowdhury <chayanchowdhury43@yahoo.com>
2024-02-24 23:21:08 +0530

    Array initialization in kotlin

---
+++ src/ArraySyntax.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("Array Data Source")
+    //initialize array
+    val num = arrayOf(1, 2, 3, 4)   //implicit type declaration
+    println(num)
+    val nums = arrayOf<Int>(1, 2, 3) //explicit type declaration
+    println(nums)
+}
