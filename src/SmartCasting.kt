06d914fb2909f2dfe52a82b6e3dcea269d0b425a
chayan chowdhury <chayanchowdhury43@yahoo.com>
2024-02-25 16:05:53 +0530

    smart casting implementation

--- src/SmartCasting.kt
+++ src/SmartCasting.kt
@@ -1,11 +1,6 @@
 fun main(){
 
-    var str1:String?="GreekCode"
-    var str2:String?="xss"
-
-    if (str2 is String){
-        println("length of string:${str2?.length}")
-    }else{
-        println("string is null")
-    }
+    val str1: String? = null
+    val str2: String? = str1 as String ?     // throw exception
+    println(str2)
 }
