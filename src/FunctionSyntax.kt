c0809d3154b621985c95f2031ed3185177d86cd1
chayan chowdhury <chayanchowdhury43@yahoo.com>
2024-02-25 18:51:19 +0530

    Function implementation in Kotlin

--- src/FunctionSyntax.kt
+++ src/FunctionSyntax.kt
@@ -3,6 +3,10 @@ fun main(){
     greet("Hello World",2024)
     var mulvalue=multiply(2,3)
     println("multiply of 2 and 3 is : ${mulvalue}")
+    val name = "chayan"
+    val standard = "VIII"
+    val roll = 25
+    student(name,standard,roll)
 }
 
 fun multiply(num:Int,num1:Int):Int{
@@ -16,4 +20,10 @@ fun greet(str:String,year:Int){
 
 fun greet(): Unit {
     println("Normal function")
+}
+
+fun student( name: String="chayan", standard: String="IX" , roll_no: Int=11 ) {
+    println("Name of the student is: $name")
+    println("Standard of the student is: $standard")
+    println("Roll no of the student is: $roll_no")
 }
