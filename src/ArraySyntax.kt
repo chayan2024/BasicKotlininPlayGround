12b96fbcfde11f394ddf03abff782660dcd3fb3c
chayan chowdhury <chayanchowdhury43@yahoo.com>
2024-02-25 10:01:14 +0530

    Initialize Array and syntax

--- src/ArraySyntax.kt
+++ src/ArraySyntax.kt
@@ -1,8 +1,40 @@
 fun main() {
-    println("Array Data Source")
+
     //initialize array
     val num = arrayOf(1, 2, 3, 4)   //implicit type declaration
     println(num)
-    val nums = arrayOf<Int>(1, 2, 3) //explicit type declaration
+
+    val nums = arrayOf<Int>(10, 20, 30,40,50,60) //explicit type declaration
     println(nums)
+
+    //iterate through loop with array
+
+    for(i in 0..num.size-1){
+        println("num: ${num[i]}")
+    }
+
+    for(i in 0..nums.size-1){
+        println("Number of array: ${nums[i]}")
+    }
+
+    //using the array constructor
+
+    val arrayName=Array(3,{i->i*1})
+    for (i in 0..<arrayName.size-1){
+        println(arrayName[i])
+    }
+
+    // build in array
+
+    val builtArray= intArrayOf(1,2,3,4,5)
+    println("builtArray: ${builtArray[3]}")
+
+    // String array
+
+    val strArray= arrayOf<String>("A","B","C")
+    println("strArray: ${strArray[2]}")
+    for(i in 0..strArray.size-1){
+        println("Number Of Values: ${strArray[i]}")
+    }
+
 }
