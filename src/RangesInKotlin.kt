909d969961d53e4cc67bbdc9f6ee5631a7ab8aca
chayan chowdhury <chayanchowdhury43@yahoo.com>
2024-02-25 16:39:25 +0530

    Range in kotlin implementation

---
+++ src/RangesInKotlin.kt
@@ -0,0 +1,25 @@
+fun main(){
+
+    println("Ranges in kotlin")
+
+    for (i in 1..10){
+        println("Numbers: ${i}")
+    }
+
+    for (i in 'a'..'f'){
+        println("charactrer: ${i}")
+    }
+
+    for(num in 100.rangeTo(1200)){
+        println(num)
+    }
+
+    for (num in 2000.downTo(1900)){
+        println(num)
+    }
+
+    for (num in 100.downTo(50)){
+        println(num)
+    }
+
+}
