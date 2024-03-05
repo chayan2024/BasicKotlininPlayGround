f0a99ad9543673e8ba953c4534a6fec8d032ec56
chayan chowdhury <chayanchowdhury43@yahoo.com>
2024-03-05 10:35:24 +0530

    Try Catch block in Kotlin

---
+++ src/TryCatchBlockInKotlin.kt
@@ -0,0 +1,20 @@
+fun main(){
+
+    val numbers= arrayOf(1,2,3,4,5,6,7,8)
+   try {
+
+       for (i in numbers.indices){
+           try {
+               var n=(0..4).random()
+               println(numbers[i+1]/n)
+
+           }catch (e:ArithmeticException){
+               println(e)
+           }
+       }
+
+   }catch (e:ArrayIndexOutOfBoundsException){
+       println(e)
+   }
+
+}
